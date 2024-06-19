9015d0a0be697917abcb0a686680a5ccb6532400
Trai Ghita <traighita@gmail.com>
2024-06-19 23:35:07 +0100

    Update basic_operations.py with new code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,6 @@
-print("Hello, Python!") 
+number1 = 10
+number2 = 5
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
